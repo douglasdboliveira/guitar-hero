d7af24c746e317cedd95ef097656b692474c5eee
Douglas de Brito <douglasdboliveira@gmail.com>
2026-05-24 22:42:43 -0300

    adicionado arquivos à pasta assets

--- js/audio.js
+++ js/audio.js
@@ -1,8 +1,8 @@
-const salomao = new Audio("baseado.mp3");
-const hino = new Audio("hino-sv.mp3");
-const pedro = new Audio("pedro.mp3");
-const shake_it_off = new Audio("shake_it_off.mp3");
-const slow_ride = new Audio("slow_ride.mp3");
+const salomao = new Audio("./assets/baseado.mp3");
+const hino = new Audio("./assets/hino-sv.mp3");
+const pedro = new Audio("./assets/pedro.mp3");
+const shake_it_off = new Audio("./assets/shake_it_off.mp3");
+const slow_ride = new Audio("./assets/slow_ride.mp3");
 
 export const musicas = [salomao, hino, pedro, shake_it_off, slow_ride];
 
